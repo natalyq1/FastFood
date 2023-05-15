c4a57c9458ec50769e086713182076bc5a86ee25
natalyq1 <onatylili@gmail.com>
2023-05-14 22:49:18 -0500

    Setting up project with some styles

---
+++ src/Form/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const MainSpace = styled.div`
+  width: 100%;
+`;
+
+export const FormSpace = styled.div`
+  width: 100%;
+`;
+
+export const LogoSpace = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 5rem;
+`;
+
+export const Img = styled.img`
+  width: 50px;
+  height: 50px;
+  margin-right: 0.8em;
+`;
